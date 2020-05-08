85d4e0e15a7e54254a3b98f5fb180cd8ed107988
Agustin Burset <agustin.burset@icloud.com>
2020-05-07 22:30:03 -0300

    Remove the magic function and make the camera setup explicit

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ const program = createProgram(gl, vertexShader, fragmentShader)
 const vertexPositionLocation = gl.getAttribLocation(program, 'vertexPosition')
 const vertexColorLocation = gl.getAttribLocation(program, 'vertexColor')
 const modelMatrixLocation = gl.getUniformLocation(program, 'modelMatrix')
+const viewMatrixLocation = gl.getUniformLocation(program, "viewMatrix")
+const projectionMatrixLocation = gl.getUniformLocation(program, "projectionMatrix")
 
 // #️⃣ Definimos la info de la geometría que vamos a dibujar
 
@@ -70,7 +72,7 @@ bindAttributeToVertexBuffer(gl, vertexColorLocation, 3, vertexColorsBuffer)
 gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
 gl.bindVertexArray(null)
 
-// #️⃣ Creamos las matrices que vamos a estar usando y valores que vamos a usar para inicializarlas
+// #️⃣ Creamos las matrices relacionadas a nuestra geometría/modelo y las inicializamos
 
 const translationMatrix = mat4.create()
 const scaleMatrix = mat4.create()
@@ -81,36 +83,57 @@ let translation = 0
 let scale = 1
 let rotation = 0
 
+mat4.fromTranslation(translationMatrix, [translation, 0, 0])
+mat4.fromScaling(scaleMatrix, [scale, scale, 1])
+mat4.fromRotation(rotationMatrix, glMatrix.toRadian(rotation), [0, 0, 1])
+
+mat4.multiply(modelMatrix, scaleMatrix, modelMatrix)
+mat4.multiply(modelMatrix, rotationMatrix, modelMatrix)
+mat4.multiply(modelMatrix, translationMatrix, modelMatrix)
+
+// #️⃣ Creamos las matrices relacionadas a la cámara y las inicializamos
+
+const viewMatrix = mat4.create()
+const projectionMatrix = mat4.create()
+
+const eye = [3, 3, 5]
+const center = [0, 0, 0]
+const up = [0, 1, 0]
+mat4.lookAt(viewMatrix, eye, center, up)
+
+const fov = glMatrix.toRadian(45)
+const aspect = canvas.width / canvas.height
+const near = 0.1
+const far = 10
+mat4.perspective(projectionMatrix, fov, aspect, near, far)
+
 // #️⃣ Establecemos el programa a usar, sus conexiónes atributo-buffer e indices a usar (guardado en el VAO)
 
 gl.useProgram(program)
 gl.bindVertexArray(vertexArray)
 
-// ✨ Magia (que en breve veremos que hace. Spoiler: configura la cámara)
-magic(gl, program, canvas)
+// #️⃣ Pasamos los valores de las matrices a los uniforms del shader de vertices
+
+gl.uniformMatrix4fv(modelMatrixLocation, false, modelMatrix)
+gl.uniformMatrix4fv(viewMatrixLocation, false, viewMatrix)
+gl.uniformMatrix4fv(projectionMatrixLocation, false, projectionMatrix)
+
+/* 📝
+ * Estar siempre atentos a que el seteo de uniforms se realize teniendo un programa en uso (via
+ * gl.useProgram). En este caso, dado que todas las matrices se inicializan y se mantienen
+ * constantes (no hay cambios en la posición de modelo, ni movimientos en la cámara, etc) estamos
+ * pudiendo optimizar el código que se ejecuta en la función render, extrayendo el seteo de
+ * uniforms, y evitando volver a pasar valores que no cambian.
+ */
 
 // #️⃣ Dibujamos la escena
 
 function render(currentTime) {
-  // Actualizamos matrices de traslación, escalado y rotación (en este caso solo va a ir cambiando la de rotación)
-  mat4.fromTranslation(translationMatrix, [translation, 0, 0])
-  mat4.fromScaling(scaleMatrix, [scale, scale, 1])
-  mat4.fromRotation(rotationMatrix, glMatrix.toRadian(rotation), [0, 0, 1])
-
-  // "Reseteamos" la modelMatrix y le aplicamos las transformaciones
-  mat4.identity(modelMatrix)
-  mat4.multiply(modelMatrix, scaleMatrix, modelMatrix)
-  mat4.multiply(modelMatrix, rotationMatrix, modelMatrix)
-  mat4.multiply(modelMatrix, translationMatrix, modelMatrix)
-
-  // Actualizamos el valor del uniform correspondiente a la modelMatrix
-  gl.uniformMatrix4fv(modelMatrixLocation, false, modelMatrix)
-
   // Limpiamos el canvas y dibujamos
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
   gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0)
 
-  // Guardamos el momento en que se dibujo este frame y solicitamos el proximo
+  // Solicitamos el proximo frame
   requestAnimationFrame(render)
 }
 
