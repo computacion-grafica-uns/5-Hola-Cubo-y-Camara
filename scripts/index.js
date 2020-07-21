bfb8dd8b573b837435b10874f450352ed98e47fa
Agustin Burset <agustin.burset@icloud.com>
2020-07-21 11:13:23 -0300

    Rename gl-utils to utils

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-import { getCanvasElement, getWebGL2Context, createShader, createProgram, createVertexBuffer, bindAttributeToVertexBuffer, createIndexBuffer, magic } from "./gl-utils.js"
+import { getCanvasElement, getWebGL2Context, createShader, createProgram, createVertexBuffer, bindAttributeToVertexBuffer, createIndexBuffer, magic } from "./utils.js"
 import { vertexShaderSourceCode, fragmentShaderSourceCode } from "./shaders.js"
 import { mat4, glMatrix } from './gl-matrix/index.js'
 
